beb8cd8ddcb1ea07a3b2c70cc7378102d6d90ae8
Fabian Baena <fabian.baena@samanagroup.com>
2025-01-15 08:52:45 -0500

    created the installation script that copies all the files into the proper location and generates the ssl self signed certificates. modified nginx to use the self signed certificates by default.

---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+INSTALL_DIR=/usr/local/samm
+SAMM_OWNER=$(whoami)
+
+OPENSSL=$(which openssl)
+if "$?" != "0"; then
+	echo "Mandatory 'openssl' command is missing" >&2
+	exit 1
+fi
+
+DOCKER=$(which docker)
+if "$?" != "0"; then
+	echo "Mandatory 'docker' command is missing" >&2
+	exit 1
+fi
+
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+set -e
+sudo mkdir -p ${INSTALL_DIR}
+sudo chown ${SAMM_OWNER}.docker ${INSTALL_DIR}
+
+cp -R ${DIR}/* ${INSTALL_DIR}
+cd ${INSTALL_DIR}
+
+
+${OPENSSL} req -x509 -newkey rsa:4096 \
+	-keyout config/nginx/samm.customer.local.key \
+	-out config/nginx/samm.customer.local.crt -sha256 \
+	-days 3650 -nodes \
+	-subj "/C=XX/ST=Florida/L=Miami/O=Samana Group/OU=ITMS/CN=samm.customer.local"
+
+echo "Environment is ready for configuration."
+echo "The next step is to go into ./env directory and edit all .env files to set passwords and other configuration"
+echo "After the configuration, the environment can be started with:"
+echo "docker compose up [-d]"
