42ec3854987e6a761faefdde74142cc7f8396d3e
dmadiedo <diegomadiedo@gmail.com>
2025-07-29 09:31:29 -0500

    Added plugin for reporting. Enabled by default since reporting is going to be needed always.

---
+++ config/grafana-plugins/install-reporting.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget https://github.com/mahendrapaipuri/grafana-dashboard-reporter-app/releases/download/v1.8.0/mahendrapaipuri-dashboardreporter-app-1.8.0.zip
+unzip mahendrapaipuri-dashboardreporter-app-1.8.0.zip
+rm -rf mahendrapaipuri-dashboardreporter-app-1.8.0.zip
